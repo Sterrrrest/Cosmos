60b1e25e79aaad3868ed82cf8014286ba523fec2
Sterrrrest <sterrrrest@gmail.com>
2023-12-03 17:18:53 +0300

    publish text to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,6 @@
-import ptbot
-bot = telegram.Bot(token='TG_TOKEN')
-print(bot.get_me())
+import telebot
+
+bot = telebot.TeleBot(token='6557313479:AAG3rsv6DzLNigHmHKDN-MCVcJRxdimdMxo')
+# print(bot.get_me())
+
+bot.send_message(chat_id='@cosmopics', text="I'm sorry Dave I'm afraid I can't do that.")
