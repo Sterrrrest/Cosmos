76c69848df5fb5ddf916e820416333edeca67b5e
Sterrrrest <sterrrrest@gmail.com>
2023-12-08 13:43:34 +0300

    Переименовал понятно списки

--- telegram_bot.py
+++ telegram_bot.py
@@ -17,14 +17,14 @@ if __name__ == '__main__':
         args = parser.parse_args()
 
         dir = 'pictures'
-        number = []
-        pictdir = []
+        picture_numbers = []
+        all_pictures = []
         for docs in os.walk(dir):
-            pictdir.append(docs[2])
-            for directory_path_number, directory_path in enumerate(pictdir[0]):
-                number.append(directory_path_number)
+            all_pictures.append(docs[2])
+            for picture_number, picture in enumerate(all_pictures[0]):
+                picture_numbers.append(picture_number)
         while True:
-            random_foto = random.sample(pictdir[0], max(number))
+            random_foto = random.sample(all_pictures[0], max(picture_numbers))
 
             for pic in random_foto:
                 with open(('pictures/' + pic), 'rb') as picture:
