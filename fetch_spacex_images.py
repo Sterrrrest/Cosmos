3eaef2ce8f04296e50659de18c2b94defb26e715
Sterrrrest <sterrrrest@gmail.com>
2023-12-12 14:26:17 +0300

    Заменил сложение на format

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from download_picture import download_picture
 
 def fetch_spacex_last_launch(launch_id):
-    response = requests.get('https://api.spacexdata.com/v5/launches/'+ launch_id)
+    response = requests.get('https://api.spacexdata.com/v5/launches/{}'.format(launch_id))
     response.raise_for_status()
     pictures = response.json()["links"]['flickr']['original']
     for picture_number, picture in enumerate(pictures):
