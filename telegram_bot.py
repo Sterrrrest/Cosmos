9590fdcf261ef3ddc28c372d272d62fd31b38392
Sterrrrest <sterrrrest@gmail.com>
2023-12-12 14:52:30 +0300

    Сделал выбор файла через random(choice)

--- telegram_bot.py
+++ telegram_bot.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         bot = telebot.TeleBot(token=token)
         parser = argparse.ArgumentParser(description='Отправляет скаченные фото в ТГ бота.'
                                                      'В качестве аргумента надо задать период отправки')
-        parser.add_argument('--count', type=int, required=False, default=14400, help='Укажите ID запуск')
+        parser.add_argument('--count', type=int, required=False, default=4, help='Укажите ID запуск')
         args = parser.parse_args()
 
         dir = 'pictures'
@@ -21,12 +21,8 @@ if __name__ == '__main__':
         all_pictures = []
         for docs in os.walk(dir):
             all_pictures.append(docs[2])
-            for picture_number, picture in enumerate(all_pictures[0]):
-                picture_numbers.append(picture_number)
         while True:
-            random_foto = random.sample(all_pictures[0], max(picture_numbers))
-
-            for pic in random_foto:
-                with open(('pictures/' + pic), 'rb') as picture:
-                    bot.send_document(chat_id=chat_id, document=picture)
-                time.sleep(args.count)
+            random_foto = random.choice(all_pictures[0])
+            with open(('pictures/' + random_foto), 'rb') as picture:
+                bot.send_document(chat_id=chat_id, document=picture)
+            time.sleep(args.count)
